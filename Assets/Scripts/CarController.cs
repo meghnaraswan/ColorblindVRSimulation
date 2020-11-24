5bd6c12ff45d0c4cbb7884afcc9eaa7c085eae1e
meghnaraswan <shauryaraswan@gmail.com>
2020-11-24 13:35:26 -0800

    finishing car driving (almost)

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+
+    private PlayerInput inputManager;
+    public List<WheelCollider> throttleWheels;
+    public List<WheelCollider> steeringWheels;
+    public float strengthCoefficient = 200000f;
+    public float maxTurn = 20f;
+
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        inputManager = GetComponent<PlayerInput>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        foreach(WheelCollider wheel in throttleWheels)
+        {
+            wheel.motorTorque = strengthCoefficient * Time.deltaTime * inputManager.Acceleration;
+            wheel.wheelDampingRate = inputManager.wheelDampening;
+        }
+
+        foreach(WheelCollider wheel in steeringWheels)
+        {
+            wheel.steerAngle = maxTurn * inputManager.Steering;
+            wheel.wheelDampingRate = inputManager.wheelDampening;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public float Acceleration
+    {
+        get { return m_Acceleration; }
+    }
+
+    public float Steering
+    {
+        get { return m_Steering; }
+    }
+
+    float m_Acceleration;
+    float m_Steering;
+
+    bool m_FixedUpdateHappened;
+
+    private bool accelerating = false;
+    private bool breaking = false;
+    private bool turningLeft = false;
+    private bool turningRight = false;
+
+    public float wheelDampening;
+
+    // Start is called before the first frame update
+//    void Start()
+//    {
+        
+//    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetPlayerInput();
+
+        if (accelerating)
+        {
+            m_Acceleration = 1f;
+            wheelDampening = 500f;
+        }
+        else if (breaking)
+        {
+            m_Acceleration = -0.5f;
+            wheelDampening = 10000f;
+        }
+        else
+        {
+            m_Acceleration = 0f;
+            wheelDampening = 5f;
+        }
+
+        if (turningLeft)
+            m_Steering = -1f;
+        else if (!turningLeft && turningRight)
+            m_Steering = 1f;
+        else
+            m_Steering = 0f;
+    }
+
+    private void GetPlayerInput()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
+        {
+            accelerating = true;
+        }
+        if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.RTouch))
+        {
+            accelerating = false;
+        }
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryThumbstickDown, OVRInput.Controller.RTouch))
+        {
+            breaking = true;
+        }
+        if (OVRInput.GetUp(OVRInput.Button.PrimaryThumbstickDown, OVRInput.Controller.RTouch))
+        {
+            breaking = false;
+        }
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryThumbstickLeft, OVRInput.Controller.RTouch))
+        {
+            turningLeft = true;
+        }
+        if (OVRInput.GetUp(OVRInput.Button.PrimaryThumbstickLeft, OVRInput.Controller.RTouch))
+        {
+            turningLeft = false;
+        }
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryThumbstickRight, OVRInput.Controller.RTouch))
+        {
+            turningRight = true;
+        }
+        if (OVRInput.GetUp(OVRInput.Button.PrimaryThumbstickRight, OVRInput.Controller.RTouch))
+        {
+            turningRight = false;
+        }
+    }
+}
